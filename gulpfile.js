05533380ef84e7b0dfdb1cef6cc799500ea016d7
Gabriel Furlan <gabrielfurlan05@gmail.com>
2017-02-13 15:10:06 -0200

    Definindo as configuracoes de estilo

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,13 @@ const gulp = require('gulp'),
 	  concatcss = require('gulp-concat-css'),
 	  stylus = require('gulp-stylus'),
 	  minifycss = require('gulp-clean-css'),
+	  mmq = require('gulp-merge-media-queries'),
 	  clean = require('gulp-clean');
 
 
 /* === TASK DEFAULT - ATIVA O OBSERVE NOS ARQUIVOS .styl === */
 gulp.task('default', () => {
-	gulp.watch('assets/stylesheets/**/*.styl', ['styl']);
+	gulp.watch('assets/css/**/*.styl', ['styl']);
 });
 
 
@@ -19,10 +20,9 @@ gulp.task('clean-css', () => {
 
 /* === TASK STYL = COMPILA OS ARQUIVOS .styl PARA .css === */
 gulp.task('styl', ['clean-css'], () => {
-	return gulp.src('assets/stylesheets/main.styl')
-	.pipe(stylus({
-     	compress: true
-     }))
+	return gulp.src('assets/css/main.styl')
+	.pipe(stylus({compress: true}))
+	.pipe(mmq())
 	.pipe(concatcss('all.min.css'))
 	.pipe(minifycss())
 	.pipe(gulp.dest('dist/stylesheets'));
