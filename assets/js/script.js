28873e5a000f65c80447c06efb9aceb073283f69
Raphael Narasaki <narasakir@gmail.com>
2017-06-06 02:00:01 -0300

    Add Javascript/ Enviando para Revisao

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,26 @@
 $(function () {
   
-  
+  	$(".scroll-top a").click(function(event){
+		event.preventDefault();
+		$('html,body').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 800); 
+	});
+
+
+	$('#banner-top').slick({
+		slide: '.item',
+		autoplay: true,
+		arrows: true,
+		dots: true,
+		responsive: [
+				{
+				breakpoint: 768,
+					settings: {
+						arrows: false
+					}
+				}
+			]
+	});
 
 });
